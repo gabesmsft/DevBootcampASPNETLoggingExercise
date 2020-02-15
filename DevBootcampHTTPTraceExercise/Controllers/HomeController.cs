d39d64720e1c2944b3886bb9ac3b6971c10bec5a
Gabe Shapiro <gabeshap@outlook.com>
2020-02-14 16:38:51 -0800

    2nd commit

--- DevBootcampHTTPTraceExercise/Controllers/HomeController.cs
+++ DevBootcampHTTPTraceExercise/Controllers/HomeController.cs
@@ -12,38 +12,57 @@ namespace DevBootcampHTTPTraceExercise.Controllers
 {
     public class HomeController : Controller
     {
-
+        
         public async Task<ActionResult> Index()
         {
-            string url = "http://gabefakesamesitetest.azurewebsites.net";
+            //This controller will send an outbound request to another service if either of the following conditions are met:
+            //1) You send a FakeHeader request header to this controller
+            //2) You don't send a X-ARR-SSL request header to this controller
+            bool validInboundRequest = (Request.Headers["FakeHeader"] != null || Request.Headers["X-ARR-SSL"] == null);
 
-            var baseAddress = new Uri(url);
-
-            using (var client = new HttpClient() { BaseAddress = baseAddress })
+            if (!validInboundRequest)
             {
-                var content = new FormUrlEncodedContent(new[]
+                ViewBag.InboundRequestCustomResponse = @"Please do one or more of the following: 1) Send a request with a FakeHeader header, or 2) Ensure that you aren't sending a X-ARR-SSL request header.";
+                ViewBag.OutboundRequestCustomResponse = "Not sent because of above error message";
+            }
+
+            else
+            {
+                ViewBag.InboundRequestCustomResponse = "Inbound request is valid";
+
+                try
                 {
-        new KeyValuePair<string, string>("fakeKey1", "fakeValue1"),
-        new KeyValuePair<string, string>("fakeKey2", "fakeValue2"),
-    });
-                
-                var result = await client.PostAsync("/", content);
-                result.EnsureSuccessStatusCode();
+                    string url = "https://devbootcampfakeexternalservice.azurewebsites.net";
+
+                    var baseAddress = new Uri(url);
+
+                    using (var client = new HttpClient() { BaseAddress = baseAddress })
+                    {
+                        var message = new HttpRequestMessage(HttpMethod.Get, "/api/fake");
+                        message.Headers.Add("FakeHeader2", Request.Url.ToString());
+                        var result = await client.SendAsync(message);
+                        
+                        if (result.StatusCode == HttpStatusCode.OK)
+                        {
+                            ViewBag.OutboundRequestCustomResponse = "Outbound request was successfully sent";
+                        }
+                        else
+                        {
+                            ViewBag.OutboundRequestCustomResponse = "An issue happened when sending the outbound request";
+                        }
+
+                        System.Diagnostics.Trace.WriteLine("Status code response of outbound request: " + result.StatusCode);
+                        string content = await result.Content.ReadAsStringAsync();
+                        System.Diagnostics.Trace.WriteLine("Response content of outbound request: " + content);
+
+                        //var result = await client.GetAsync("/api/fake");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.OutboundRequestCustomResponse = "An exception happened when trying to send the outbound request";
+                }
             }
-
-            using (var handler = new HttpClientHandler { UseCookies = false })
-            using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
-            {
-                var message = new HttpRequestMessage(HttpMethod.Post, "/Default.aspx");
-                message.Headers.Add("Connection", "keep-alive");
-                message.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36");
-                message.Headers.Add("Accept-Encoding", "gzip, deflate");
-
-                var result = await client.SendAsync(message);
-                System.Diagnostics.Trace.WriteLine(result.StatusCode);
-                System.Diagnostics.Trace.WriteLine(result.Content);
-            }
-
             return View();
         }
     }
